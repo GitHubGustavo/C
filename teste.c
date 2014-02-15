a2e2927b37e062fd159373404448066fa8862b1c
Gustavo Carvalho Souza <gustavo.sdpp@gmail.com>
2014-02-15 17:09:05 -0200

    Primeiro commit

---
+++ teste.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int fibonacci (int anterior, int atual){
+	if (atual==0){
+		return 0;
+	}
+	if (atual==1){
+
+}
+
+
+
+}
+
+
+main (){
+
+	
+
+
+
+
+}
